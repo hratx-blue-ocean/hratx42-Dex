f970e53184d1f17ca40a4393251abc1056a5df37
zonagilreath <bryson.gilreath@gmail.com>
2019-08-11 18:19:40 -0500

    implement dynamic room connection with socket.io

--- server/socketsExperiments/server.js
+++ server/socketsExperiments/server.js
@@ -2,46 +2,29 @@ const WebSocket = require('ws');
 const http = require('http');
 const path = require('path')
 
-const app = require('express')();
+const express = require('express');
+const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-io.on('connection', () => { /* … */ });
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-wss.on('connection', ws => {
-  ws.room=null;
-  ws.send(JSON.stringify({msg:"connected"}));
+ // const dynamicTableSpace = io.of(/\/table\/\d+$/)
+const tableSpace = io.of('/table/')
+tableSpace.on('connection', (socket) => {
   console.log('connected');
-  ws.on('message', messageJSON => {
-    console.log('message: ',messageJSON);
-    //try{
-    var message=JSON.parse(messageJSON);
-    //}catch(e){console.log(e)}
-    if(message.join){ws.room = message.join}
-    if(message.room){broadcast(message);}
-    if(message.msg){console.log('message: ',message.msg)}
-  })
 
-  ws.on('error', e => console.log(e))
-  ws.on('close', e => console.log('websocket closed' + e))
+  // broadcast to all clients in the given sub-namespace
+  tableSpace.emit('update', 'hello');
+  socket.on('room', function(room) {
+    socket.join(room);
+  });
+  socket.on('post', data => console.log(data))
+});
 
-})
-
-function broadcast(message){
-  //console.log(wss.clients);
-  wss.clients.forEach(client=>{
-    //if(client.room.indexOf(message.room) > -1){
-    // console.log('client rooms', client.room[0]);
-    console.log('client room', client.room);
-    if(client.room === message.room){
-      client.send(JSON.stringify(message))
-    }
-  })
-}
-server.listen(3000, (err) => {
+server.listen(5000, (err) => {
   if (err) console.error(err);
-  else console.log('listening on 3000');
+  else console.log('listening on 5000');
 });
 
 // const server=http.createServer(app);
@@ -50,4 +33,3 @@ server.listen(3000, (err) => {
 // server.listen(webPort, function(){
 //  console.log('Web server start. http://localhost:' + webPort );
 // });
-// const wss = new WebSocket.Server({ server });
