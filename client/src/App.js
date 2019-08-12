ad909781d3dbf60c7f7547706497d7189c05edbd
Michael Bergeron <michaelbergeron@Michaels-MacBook-Pro.local>
2019-08-12 15:53:41 -0500

    working on new talbe modal

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,35 @@ import Landing from './components/Landing.js';
 import Dashboard from './components/Dashboard.js';
 import Profile from './components/Profile.js';
 import NavBar from './components/NavBar.js'
-import Table from './components/Table.js'
+import Table from './components/Table.js';
+import TableThumbnails from './components/TableThumbnails.js';
 // import './App.css';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userid: '',
-      boards: [],
-    };
-    this.api = `http://localhost:8000/api/example`;
+      userid: 0,
+      boards: [
+        {"id":0,
+          "name":"firewall",
+          "photo":"http://lorempixel.com/640/480/city",
+          "users":
+            [
+              {"id":0,
+                "email":"Jarrod_Stroman58@yahoo.com",
+                "name":"Oscar_Konopelski",
+                "photo":"http://lorempixel.com/640/480/animals"},
+              {"id":1,
+                "email":"Jeromy.Corkery@yahoo.com",
+                "name":"Elsie.Stamm69",
+                "photo":"http://lorempixel.com/640/480/animals"},
+              {"id":2,
+                "email":"Gerardo_Schowalter67@yahoo.com",
+                "name":"Kiera_Renner98",
+                "photo":"http://lorempixel.com/640/480/animals"},{"id":3,"email":"Landen_Torphy@gmail.com","name":"Zelda.Kertzmann21","photo":"http://lorempixel.com/640/480/animals"},{"id":4,"email":"Onie_Donnelly17@yahoo.com","name":"Vladimir.Jacobs25","photo":"http://lorempixel.com/640/480/animals"},{"id":5,"email":"Stephan38@gmail.com","name":"Will.Wiegand50","photo":"http://lorempixel.com/640/480/animals"},{"id":6,"email":"Timmy67@hotmail.com","name":"Hayley.Orn","photo":"http://lorempixel.com/640/480/animals"},{"id":7,"email":"Gloria75@hotmail.com","name":"Makayla_Waters57","photo":"http://lorempixel.com/640/480/animals"},{"id":8,"email":"Annalise79@yahoo.com","name":"Fay_Orn","photo":"http://lorempixel.com/640/480/animals"},{"id":9,"email":"Gregory_Gutkowski47@gmail.com","name":"Sierra.Roberts75","photo":"http://lorempixel.com/640/480/animals"},{"id":10,"email":"Kylie57@yahoo.com","name":"Mark39","photo":"http://lorempixel.com/640/480/animals"},{"id":11,"email":"Shanon53@gmail.com","name":"Josiane66","photo":"http://lorempixel.com/640/480/animals"}]}]
   }
+}
   componentDidMount() {
     // fetch(this.api)
     //   .then(res => res.json())
@@ -35,6 +52,7 @@ export default class App extends Component {
           <Route path="/profile" component={ Profile } />
           <Route path="/table" component={ Table } />
         </Router>
+        <TableThumbnails />
       </>
     );
   }
--- client/src/components/TableThumbnails.js
+++ client/src/components/TableThumbnails.js
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Card, Button, Row, Col, Table, Form } from 'react-bootstrap';
+import styles from '../modules/tableThumbnail.module.css';
 
 export default function TableThumbnails(props) {
+  const [tableName, setTableName] = useState('');
+  const [playerName, setPlayerName] = useState('');
+  const [descName, setDescName] = useState('');
   return (
-    <div>
-      thumbnails
+    <div className = {`${styles.tableCreate} container`}>
+      <Card>
+        <div>
+          <div className = {styles.header}>Create/Edit<Button className = {`${styles.buttonx} float-right`} variant = 'light'>x</Button></div>
+          
+        </div>
+        <hr/>
+        <Row>
+          <Col md = {{offset: 1}} className = {styles.column}>
+            <span>Table Name:</span>
+            <div className = {styles.space}></div>
+            <div>
+              <input onChange = {(e) => setTableName(e.target.value)} type="text" placeholder = 'Enter table name'/>
+            </div>
+            <div className = {styles.space}></div>
+            <div>Description</div>
+            <div className = {styles.space}></div>
+            <textarea className = {styles.description} onChange = {(e) => setDescName(e.target.value)} type="text" placeholder = 'Enter table name'/>
+            <div className = {styles.space}></div>
+            <div>Invite Players</div>
+            <div className = {styles.space}></div>
+            <div>
+              <input onChange = {(e) => setPlayerName(e.target.value)} type="text" placeholder = 'Enter table name'/>
+              <Button onClick = {() => props.newPlayer(playerName)} className = {styles.plusButton} size = 'sm' variant= 'success'>+</Button>
+            </div>
+            <div className = {styles.space}></div>
+            <Button onClick = {() => props.saveTable(tableName, descName)} variant= 'success'>Save</Button>
+            <div className = {styles.space}></div>
+          </Col>
+          <Col>
+            <span>Administrator(s): </span>
+            <div className = {styles.space}></div>
+            {/* map through administrators */}
+            <div>the dude</div>
+            <div className = {styles.space}></div>
+            <div>
+              <Table size = 'sm'>
+                <thead>
+                  <tr>
+                    <th>Players</th>
+                    <th>Edit Admin</th>
+                    <th>Remove</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {/* map through all players */}
+                  <tr>
+                    <td>brooke</td>
+                    <td><Form.Check  type = 'checkbox'/></td>
+                    <td onClick = {() => {}} className = {styles.xbutton}>x</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </div>
+          </Col>
+          <Col md={1}></Col>
+        </Row>
+      </Card>
     </div>
   )
 }
