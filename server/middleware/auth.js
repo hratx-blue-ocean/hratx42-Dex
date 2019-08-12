29bfab60ae59f7a52a99637061d4ad44f9f93995
DJ <dongjae93@gmail.com>
2019-08-12 14:58:49 -0500

    setup for basic authentication middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+//connect to db later
+const db;
+
+const auth = (req, res, next) => {
+  const { email, password } = req.body;
+  
+  //get user email
+  db.getUserLoginInfo(email)
+  //if user email not found send error message
+  .catch((noUser) => {
+    res.send({
+      success: false,
+      error: "Email not found"
+    });
+  })
+  //if user email found, compare password
+  .then(async (user) => {
+    const match = await bcrypt.compare(password, user.password);
+    if(match) {
+      res.send({success: true})
+    } else {
+      res.send({
+        success: false,
+        error: "Password is incorrect"
+      })
+    }
+  })
+
+}
+
+module.exports = { auth }
