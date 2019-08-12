beaeaa26b0c8aa289721be7312ee6ef94cd2fcf8
Michael Bergeron <michaelbergeron@Michaels-MacBook-Pro.local>
2019-08-12 18:33:22 -0500

    made Table/Deck/CardThumbnails work with fake data dynamically

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Card, Button } from 'react-bootstrap';
 import Controls from './Controls';
-import Deck from './Deck'
+import Deck from './Deck';
+import mockHttp from '../../services/http/__mocks__/mockHttp';
 
 export default class Table extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      decks: [{}],
+      decks: [],
       tickets: [{}],
       users: [],
       filterBy: '',
@@ -15,7 +16,10 @@ export default class Table extends Component {
     };
   }
   componentDidMount() {
-
+    mockHttp.getDecks(0)
+    .then((res) => {
+      this.setState({decks: res})
+    })
   }
 
   saveTable(tableName, descName) {
@@ -27,16 +31,18 @@ export default class Table extends Component {
   }
 
   render() {
+    console.log(this.state.decks.length)
     return (
       <div>
         <Controls />
-        
         {/* for each deck, create a deck */}
-        <Deck />
-        <Deck />
-        <Card style = {{width: '75%', height: '150px'}}>
-          <Button style = {{height: '75px', width: '75px'}} variant='success'>Add New Deck</Button>
-        </Card>
+        {this.state.decks.length > 0 ? (<>
+          {this.state.decks.map((deck) => <Deck key = {deck.id} deck = {deck} />)}
+            <Card style = {{width: '75%', height: '150px'}}>
+              <Button style = {{height: '75px', width: '75px'}} variant='success'>Add New Deck</Button>
+            </Card>
+        </>) : (<></>)}
+        
         {/* chat box??? */}
       </div>
     )
