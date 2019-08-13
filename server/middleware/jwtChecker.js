76d9c434861e16fe1ab847a241b5ede4eb202042
DJ <dongjae93@gmail.com>
2019-08-13 11:19:29 -0500

    assigned userid to req.user

--- server/middleware/jwtChecker.js
+++ server/middleware/jwtChecker.js
@@ -1,21 +1,28 @@
 const jwt = require('jsonwebtoken');
+// const config =require('./config.js');
+const config = {secret: "supersecuresecret"};
 
 const checkToken = (req, res, next) => {
+
   let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
-  if (token.startsWith('Bearer ')) {
-    // Remove Bearer from string
-    token = token.slice(7, token.length);
-  }
+  console.log('token', token)
+  // if (token.startsWith('Bearer ')) {
+  //   // Remove Bearer from string
+  //   token = token.slice(7, token.length);
+  // }
 
   if (token) {
     jwt.verify(token, config.secret, (err, decoded) => {
       if (err) {
+        console.log('error veryfying jwt', err);
         return res.status(403).json({
           success: false,
           message: 'Token is not valid'
         });
       } else {
-        req.decoded = decoded;
+        console.log('hi');
+        req.user = decoded;
+        console.log('req.user', req.user);
         next();
       }
     });
