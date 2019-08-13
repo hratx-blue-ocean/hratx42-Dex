69765e704e2c0b3486b58e5b9ab265ba918ddf89
Brooke <brooke.snellings@gmail.com>
2019-08-13 11:58:37 -0500

    Add error catching

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -7,9 +7,13 @@ router.get('/:id', (req, res) => {
   const cardId = req.params.id;
   //if req.user && user owns table associated with card
   //get card
-  db.getCardByID(cardId).then(response => {
-    res.status(200).json(response);
-  });
+  db.getCardByID(cardId)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(404);
+    });
 });
 
 //create new card
@@ -17,9 +21,13 @@ router.post('/', (req, res) => {
   const card = req.body;
   //if req.user
   //post card
-  db.createNewCard(card).then(response => {
-    res.status(200).json(response);
-  });
+  db.createNewCard(card)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(404);
+    });
 });
 
 //update card
@@ -28,9 +36,13 @@ router.put('/:id', (req, res) => {
   const id = req.params.id;
   //if req.user && user owns card's table
   //update table
-  db.updateCard(card, id).then(response => {
-    res.status(200).json(response);
-  });
+  db.updateCard(card, id)
+    .then(response => {
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      res.status(404);
+    });
 });
 
 //delete card by cardID
@@ -38,9 +50,13 @@ router.delete('/:id', (req, res) => {
   const id = req.params.id;
   //if req.user && user owns table of card
   //delete card
-  db.deleteCard(id).then(response => {
-    res.status(200).send(`Deleted card ${id}`);
-  });
+  db.deleteCard(id)
+    .then(response => {
+      res.status(200).send(`Deleted card ${id}`);
+    })
+    .catch(err => {
+      res.status(404);
+    });
 });
 
 module.exports = router;
