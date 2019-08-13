191fcaa840faf0e6e72bb49df6e6bb98d7fb1cd8
zonagilreath <bryson.gilreath@gmail.com>
2019-08-13 15:38:39 -0500

    add basic card db methods in cards model

--- db/models/cards.js
+++ db/models/cards.js
@@ -33,5 +33,28 @@ const cardsModel = {
             group by c.id;
         `, [deckId])
         return cards;
+    },
+    async get(id){
+        const query = 'select * from cards where id = $1'
+        const {rows: cards} = pgClient.query(query, id);
+        return cards[0];
+    },
+    // async post(deck){
+    //     const query = 'Insert into decks (id, table_id, title, table_index) values (default, $1, $2, 1)';
+    //     const values = [deck.table_id, deck.title]
+    //     const result = await pgClient.query(query, values);
+    //     return result
+    // },
+    // async put(deck){
+    //     const query = 'update decks set title = $1 where id = $2';
+    //     const values = [deck.title, deck.id]
+    //     const result = await pgClient.query(query, values);
+    //     return result
+    // },
+    async delete(id){
+        const query = 'delete from cards where id = $1';
+        const values = [id]
+        const result = await pgClient.query(query, values)
+        return result;
     }
 }
