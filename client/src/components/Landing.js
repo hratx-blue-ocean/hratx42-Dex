26ace6a7cd0f155ef7f95dfb46f8dbbe1936c7d8
Brooke <brooke.snellings@gmail.com>
2019-08-13 09:44:54 -0500

    Add Login

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Register from './Register/Register.js';
+import Login from './Login';
 
 export default function Landing(props) {
   return (
     <div>
+      <Login />
       Landing Page is here
       <Register />
     </div>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from 'react';
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 
 export default function Login(props) {
   return (
     <div>
-      Login
+      <Navbar bg='light' expand='lg'>
+        <Navbar.Brand href='#home' style={{ marginBottom: '22px' }}>
+          <h2>Dex</h2>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='mr-auto' />
+          <Form
+            style={{ marginRight: '10px', marginBottom: '24px' }}
+            onChange={e => {
+              console.log(e.target.value);
+              // capture email address
+            }}
+          >
+            Email
+            <FormControl type='email' className='mr-sm-2' />
+          </Form>
+          <Form
+            style={{ marginRight: '10px' }}
+            onChange={e => {
+              console.log(e.target.value);
+              // capture password
+            }}
+          >
+            Password
+            <FormControl type='password' placeholder='' className='mr-sm-2' />
+            <small>Forgot Password?</small>
+          </Form>
+          <Form style={{ marginRight: '25px' }}>
+            <Button
+              variant='success'
+              className='mr-sm-2'
+              onClick={() => {
+                console.log('clicked log in');
+                // send get request for user information to render dashboard
+              }}
+            >
+              log in
+            </Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
-  )
+  );
 }
