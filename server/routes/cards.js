163df026fb8d3364040c3711930007a550265e20
Brooke <brooke.snellings@gmail.com>
2019-08-13 11:52:19 -0500

    Write card db queries

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -1,35 +1,46 @@
-var express = require('express')
-var router = express.Router()
-// const db = require('../../db/hosteddb');
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/hosteddb');
 
-router.get('/:id', (req, res)=>{
-    const cardId = req.params.id;
-    //if req.user && user owns table associated with card
-        //get card
-    res.status(200).send(`Card ${cardId}`)
-})
+//get card info by cardID
+router.get('/:id', (req, res) => {
+  const cardId = req.params.id;
+  //if req.user && user owns table associated with card
+  //get card
+  db.getCardByID(cardId).then(response => {
+    res.status(200).json(response);
+  });
+});
 
-router.post('/', (req, res)=>{
-    const card = req.body;
-    //if req.user
-        //post card
-    res.status(200).send(JSON.stringify(card))
-})
+//create new card
+router.post('/', (req, res) => {
+  const card = req.body;
+  //if req.user
+  //post card
+  db.createNewCard(card).then(response => {
+    res.status(200).json(response);
+  });
+});
 
-router.put('/:id', (req, res)=>{
-    const card = req.body;
-    const id = req.params.id
-    card.id=id
-    //if req.user && user owns card's table
-        //update table
-    res.status(200).send(JSON.stringify(card));
-})
+//update card
+router.put('/:id', (req, res) => {
+  const card = req.body;
+  const id = req.params.id;
+  //if req.user && user owns card's table
+  //update table
+  db.updateCard(card, id).then(response => {
+    res.status(200).json(response);
+  });
+});
 
-router.delete('/:id', (req, res)=>{
-    const id = req.params.id
-    //if req.user && user owns table of card
-        //delete table
+//delete card by cardID
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  //if req.user && user owns table of card
+  //delete card
+  db.deleteCard(id).then(response => {
     res.status(200).send(`Deleted card ${id}`);
-})
+  });
+});
 
 module.exports = router;
