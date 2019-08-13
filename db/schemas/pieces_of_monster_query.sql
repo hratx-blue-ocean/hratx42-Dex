76dacee4906360636e858c3e05f355858541dd2b
zonagilreath <bryson.gilreath@gmail.com>
2019-08-12 23:46:42 -0500

    join query between cards and labels

--- db/schemas/pieces_of_monster_query.sql
+++ db/schemas/pieces_of_monster_query.sql
@@ -16,3 +16,18 @@ from cards c join (
     from cards_labels
     group by card_id
 ) l on c.id = l.card_id;
+
+select 
+    c.id, 
+    c.title as card_title, 
+    c.description as card_description, 
+    c.updated_at,
+    c.created_at, 
+    c.weight as card_weight,
+    c.impact as card_impact,
+    l.label_name,
+    l.color
+from
+    cards c 
+    join cards_labels cl on c.id = cl.card_id
+    join labels l on cl.label_id = l.id;
