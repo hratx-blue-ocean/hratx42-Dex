5c9d9bb71959bacd653350abbb303371f6be8b46
zonagilreath <bryson.gilreath@gmail.com>
2019-08-12 23:56:28 -0500

    create some elements of monster query

--- db/schemas/pieces_of_monster_query.sql
+++ db/schemas/pieces_of_monster_query.sql
@@ -25,9 +25,22 @@ select
     c.created_at, 
     c.weight as card_weight,
     c.impact as card_impact,
-    l.label_name,
-    l.color
+    array[l.label_name, l.color] as label
 from
     cards c 
     join cards_labels cl on c.id = cl.card_id
     join labels l on cl.label_id = l.id;
+
+select 
+    c.id, 
+    c.title as card_title, 
+    c.description as card_description, 
+    c.updated_at,
+    c.created_at, 
+    c.weight as card_weight,
+    c.impact as card_impact,
+    array[cast(u.id as varchar), u.name] as user
+from
+    cards c 
+    join cards_members cm on c.id = cm.card_id
+    join users u on cm.user_id = u.id;
