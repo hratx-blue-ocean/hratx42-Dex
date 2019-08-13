d704daf8eb42547b9858cb2aebda46bc7f57a719
Brooke <brooke.snellings@gmail.com>
2019-08-13 15:53:29 -0500

    Add all card query methods to models

--- db/models/cards.js
+++ db/models/cards.js
@@ -1,5 +1,44 @@
 const pgClient = require('../hosteddb');
 
+const cardsModel = {
+  async getCardsByDeckId(deckId) {
+    const cards = pgClient.query(
+      `
+          select 
+              c.id as card_id,
+              c.title as card_title,
+              c.description as card_description,
+              c.updated_at as card_updated,
+              c.created_at as card_created,
+              c.weight as card_weight,
+              c.impact as card_impact,
+              array_agg(
+                  json_build_object(
+                      'member_id', cast(u.id as varchar),
+                      'member_name', u.name
+                  )
+              ) as cards_members,
+              array_agg(
+                  json_build_object(
+                      'label_name', l.label_name,
+                      'color', l.color
+                  )
+              ) as card_labels
+          from
+              cards c 
+              join cards_members cm on c.id = cm.card_id
+              join users u on cm.user_id = u.id
+              join cards_labels cl on c.id = cl.card_id
+              join labels l on cl.label_id = l.id
+          where c.deck_id = $1 
+          group by c.id;
+      `,
+      [deckId]
+    );
+    return cards;
+  }
+};
+
 // get card info by cardID
 const getCardByID = async cardID => {
   const cardInfo = await pgClient.query(
@@ -56,6 +95,7 @@ const deleteCard = async cardID => {
 };
 
 module.exports = {
+  cardsModel,
   getCardByID,
   createNewCard,
   updateCard,
