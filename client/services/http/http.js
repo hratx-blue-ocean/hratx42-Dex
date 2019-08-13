4a1d97485c1b88f57fdc12a22470c054ac84275d
Brian Embry <b.a.embry@gmail.com>
2019-08-12 21:59:36 -0500

    Final commit before leaving branch. Forgot what I did

--- client/services/http/http.js
+++ client/services/http/http.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
+import global from '../../utils/global';
 
 const tryAxios = async function(endpoint, method, payload){
     try {
         const response = await axios[method](endpoint,payload)
         return response.data
       } catch (error) {
-        console.error(error)
+        global.flash(error.message, 2000)
       }
 }
 
@@ -22,10 +23,10 @@ const http ={
         }
     },
     auth: {
-        post(email, password){
-            const jwt = tryAxios('/api/auth', 'post', {email, password})
+        async post(email, password){
+            const jwt = await tryAxios('/api/auth', 'post', {email, password})
             //do something with jwt
-            //redirect
+            window.location = '/dashboard'
         },
     },
     tables: {
