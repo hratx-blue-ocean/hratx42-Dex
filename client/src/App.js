0a1cf37d38bb5abdbad59c27f3bcf595fd398c5a
Eisenbartl <Eisenbartl@yahoo.com>
2019-08-14 11:48:07 -0500

    branch test

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ export default class App extends Component {
       profile: {
         editName: '',
         editEmail: '',
-        editPasswor: ''
+        editPasswo: ''
       },
       
       flash: {
