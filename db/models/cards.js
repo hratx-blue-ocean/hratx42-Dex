e9d48b560b6d9190a4f92d150a855b4b932254ed
zonagilreath <bryson.gilreath@gmail.com>
2019-08-14 16:14:39 -0500

    implement add label to card endpoint

--- db/models/cards.js
+++ db/models/cards.js
@@ -2,69 +2,69 @@ const pgClient = require('../hosteddb');
 
 const cardsModel = {
   async getCardsByDeckId(deckId) {
-      const {rows: cards} = await pgClient.query(`
-          select 
-              c.id as card_id,
-              c.title as card_title,
-              c.description as card_description,
-              c.updated_at as card_updated,
-              c.created_at as card_created,
-              c.weight as card_weight,
-              c.impact as card_impact,
-              array_agg(
-                  json_build_object(
-                      'member_id', cast(u.id as varchar),
-                      'member_name', u.name
-                  )
-              ) as cards_members,
-              array_agg(
-                  json_build_object(
-                      'label_name', l.label_name,
-                      'color', l.color
-                  )
-              ) as card_labels
-          from
-              cards c 
-              join cards_members cm on c.id = cm.card_id
-              join users u on cm.user_id = u.id
-              join cards_labels cl on c.id = cl.card_id
-              join labels l on cl.label_id = l.id
-          where c.deck_id = $1 
-          group by c.id;
-      `, [deckId])
-      return cards;
+    const query = `
+        select 
+          c.id as card_id,
+          c.title as card_title,
+          c.description as card_description,
+          c.updated_at as card_updated,
+          c.created_at as card_created,
+          c.weight as card_weight,
+          c.impact as card_impact,
+          array_agg(
+            json_build_object(
+              'member_id', cast(u.id as varchar),
+              'member_name', u.name
+            )
+          ) as cards_members,
+          array_agg(
+            json_build_object(
+                'label_name', l.label_name,
+                'color', l.color
+            )
+          ) as card_labels
+        from
+          cards c 
+          join cards_members cm on c.id = cm.card_id
+          join users u on cm.user_id = u.id
+          join cards_labels cl on c.id = cl.card_id
+          join labels l on cl.label_id = l.id
+        where c.deck_id = $1 
+        group by c.id;`
+    const {rows: cards} = await pgClient.query(query, [deckId])
+    return cards;
   },
   async getCardByID(id){
       const query = `
-          select 
-              c.id as card_id,
-              c.title as card_title,
-              c.description as card_description,
-              c.updated_at as card_updated,
-              c.created_at as card_created,
-              c.weight as card_weight,
-              c.impact as card_impact,
-              array_agg(
-                  json_build_object(
-                      'member_id', cast(u.id as varchar),
-                      'member_name', u.name
-                  )
-              ) as cards_members,
-              array_agg(
-                  json_build_object(
-                      'label_name', l.label_name,
-                      'color', l.color
-                  )
-              ) as card_labels
-          from
-              cards c 
-              join cards_members cm on c.id = cm.card_id
-              join users u on cm.user_id = u.id
-              join cards_labels cl on c.id = cl.card_id
-              join labels l on cl.label_id = l.id
-          where c.id = $1 
-          group by c.id;
-      `
+        select 
+          c.id as card_id,
+          c.title as card_title,
+          c.description as card_description,
+          c.updated_at as card_updated,
+          c.created_at as card_created,
+          c.weight as card_weight,
+          c.impact as card_impact,
+          array_agg(
+            json_build_object(
+              'member_id', cast(u.id as varchar),
+              'member_name', u.name
+            )
+          ) as cards_members,
+          array_agg(
+            json_build_object(
+                'label_name', l.label_name,
+                'color', l.color
+            )
+          ) as card_labels
+        from
+          cards c 
+          join cards_members cm on c.id = cm.card_id
+          join users u on cm.user_id = u.id
+          join cards_labels cl on c.id = cl.card_id
+          join labels l on cl.label_id = l.id
+        where c.id = $1 
+        group by c.id;
+      `;
       const {rows: cards} = pgClient.query(query, id);
       return cards[0];
   },
@@ -126,6 +126,13 @@ const cardsModel = {
     const {rows: result} = await pgClient.query(query, [cardId, memberId]);
     const deletedMemberId = await result[0];
     return deletedMemberId;
+  },
+  async addLabelToCard(cardId, labelId){
+    const query = `insert into cards_labels (card_id, label_id)
+                   values ($1, $2) returning label_id;`;
+    const {rows: result} = await pgClient.query(query, [cardId, memberId]);
+    const insertedLabelId = await result[0].member_id;
+    return insertedLabelId;
   },
   // delete card
   async deleteCard(cardID){
