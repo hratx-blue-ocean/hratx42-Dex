c553925eef39da37522c34c202d6686b0de32839
DJ <dongjae93@gmail.com>
2019-08-14 10:54:20 -0500

    deleted console log

--- server/routes/users.js
+++ server/routes/users.js
@@ -21,7 +21,7 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res, next) => {
     const { email, password, name } = req.body;
     //post user to db if she doesn't already exist
-    console.log('creating user', req.body)
+    // console.log('creating user', req.body)
     db.getUserInfoByEmail(email)
     .then((result) => {  
         if(result.rowCount === 0) {     //if email does not exist create user
