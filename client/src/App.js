9c4b7550de8ccc6edc9d72013b22fd16ddd59799
Eisenbartl <Eisenbartl@yahoo.com>
2019-08-14 15:16:20 -0500

    changed dashboard edit profile post request, to a put request

--- client/src/App.js
+++ client/src/App.js
@@ -102,8 +102,8 @@ export default class App extends Component {
 
   submitProfileChanges() {
     http.users
-      .post(
-        this.state.profile.editName,
+      .put(
+        this.state.userId,
         this.state.profile.editEmail,
         this.state.profile.editPassword,
       )
