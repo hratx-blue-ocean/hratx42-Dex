90ed71f3280ea0f2436bf1a0c4adb1e3c2c92592
Michael-Bergeron <45839261+Michael-Bergeron@users.noreply.github.com>
2019-08-14 16:42:16 -0500

    Update hosteddb.js

--- db/hosteddb.js
+++ db/hosteddb.js
@@ -1,12 +1,9 @@
 const path = require('path');
 
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-<<<<<<< HEAD
-console.log(require('dotenv').config({ path: path.resolve(__dirname, '../.env') }))
-=======
 console.log(require('dotenv').config({ path: path.resolve(__dirname, '../.env') }));
 console.log(process.env.DB_HOST, process.env.DB_USERNAME, process.env.DB_PASSWORD)
->>>>>>> ecf4a687b980f5232d9f09b2b2f3e89c0820b4c9
+
 const pg = require('pg');
 
 const pgClient = new pg.Pool({
