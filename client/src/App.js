3e1bff13ad0e767cfc5ec49d2ec35c8e8ad1786e
Eisenbartl <Eisenbartl@yahoo.com>
2019-08-14 11:46:53 -0500

    minor change to test git branch

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ export default class App extends Component {
       profile: {
         editName: '',
         editEmail: '',
-        editPassword: ''
+        editPasswor: ''
       },
       
       flash: {
