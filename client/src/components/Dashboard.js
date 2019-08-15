fb732b15cfa778b80beff13aced56f3605214bcf
zonagilreath <bryson.gilreath@gmail.com>
2019-08-15 17:37:23 -0500

    building out change profile form, need to fix other bugs first

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,32 +1,12 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
 import { userInfo } from 'os';
+import ProfileEditForm from './ProfileEditForm.jsx';
 
 export default function Dashboard(props) {
   return (
     <div className="dashboardContainer">
-      {/* edit profile area */}
-      <div className="dashboardProfileForm" >
-        <h2>edit profile</h2>
-        <form>
-          <div className="dashboardNameInput">
-            <h4>name</h4>
-            <input type="text" className="name" placeholder={props.user.name} onChange={e => {props.changeProfileName(e)}} value={props.editProfileName} />
-          </div>
-
-          <div className="dashboardEmailInput">
-            <h4>email</h4>
-            <input type="email" className="email" placeholder={props.user.email} onChange={e => {props.changeProfileEmail(e)}} value={props.editProfileEmail} />
-          </div>
-
-          <div className="dashboardPasswordInput">
-            <h4>password</h4>
-            <input type="password" className="name" placeholder="password" onChange={e => {props.changeProfilePassword(e)}} value={props.editProfilePassword} />
-          </div>
-          <button type="submit" className="dashboardFormBtn" onClick={() => {props.submitProfileChanges()}}>save changes</button>
-        </form>
-      </div>
-
+      <ProfileEditForm user={props.user} />
       {/* tables */}
       <div className="dashboardTables">
         <h2>your tables</h2>
