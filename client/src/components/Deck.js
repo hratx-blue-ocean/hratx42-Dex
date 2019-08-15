b8fee400fe855bb727f7480af2cf4078438bbbb0
Brooke <brooke.snellings@gmail.com>
2019-08-15 18:12:08 -0500

    Add deck scroll functionality

--- client/src/components/Deck.js
+++ client/src/components/Deck.js
@@ -15,47 +15,45 @@ export default function Deck(props) {
   let cards = [];
   if (props.deck.cards) {
     if (props.filterBy === 'Filter') {
-      cards = props.deck.cards.slice(0, 6);
+      cards = props.deck.cards.slice(0, 20);
     } else {
       props.deck.cards.forEach(card => {
         card.cards_members.forEach(member => {
-          if (member.member_name){
-          if (member.member_name.includes(props.filterBy)) {
-            cards.push(card);
-          }}
+          if (member.member_name) {
+            if (member.member_name.includes(props.filterBy)) {
+              cards.push(card);
+            }
+          }
         });
       });
     }
-}
+  }
   return (
-    <div style={{ width: '75%' }}>
-      <div>
-        <Card style={{ backgroundColor: '#eee' }}>
-          <Card.Header>
-            <span>
-              <span style={{ paddingRight: '30px' }}>{props.deck.title}</span>
-              <Button
-                variant='outline-success'
-                onClick={() => setShowEditDeck(true)}
-              >
-                Edit/Delete Deck
+    <div id="deckWrapper">
+      <div id="deckContent" >
+        <Card.Header id="deckTitle">{props.deck.title}
+          <Button
+            variant='outline-success'
+            onClick={() => setShowEditDeck(true)}
+          >
+            Edit Deck
               </Button>
-            </span>
-          </Card.Header>
-            <Card.Body className = 'row'>
-            {cards.map((singleCard, cardIndex) => 
-              <div key = {Math.random()}>
-                <div style = {{paddingLeft: '160px'}}></div>
+        </Card.Header>
+        <div id="deckScrollbar" style={{ overflow: 'scroll' }}>
+          <Card.Body className='row'>
+            {cards.map((singleCard, cardIndex) =>
+              <div key={Math.random()}>
+                <div style={{ paddingLeft: '160px' }}></div>
 
-                <CardThumbnail deckIndex = {props.deckIndex}
-                              cardIndex = {cardIndex}
-                              singleCard = {singleCard} 
-                              deckTitle={props.deck.title} 
-                              deckNames={props.deckNames} 
-                              moveCard = {props.moveCard} 
-                              editCard={props.editCard}
-                              users={props.users} 
-                              labels={props.labels}/>
+                <CardThumbnail deckIndex={props.deckIndex}
+                  cardIndex={cardIndex}
+                  singleCard={singleCard}
+                  deckTitle={props.deck.title}
+                  deckNames={props.deckNames}
+                  moveCard={props.moveCard}
+                  editCard={props.editCard}
+                  users={props.users}
+                  labels={props.labels} />
               </div>
             )}
             <div style={{ paddingLeft: '20px' }} />
@@ -66,37 +64,39 @@ export default function Deck(props) {
               deckNames={props.deckNames}
               closeModal={handleClose.bind(this)}
               newCardData={props.newCardData}
-              users={props.users} 
-              labels={props.labels}/>
+              users={props.users}
+              labels={props.labels} />
 
             <Button variant='outline-success' onClick={() => handleShow()}>
               Add New Card
             </Button>
           </Card.Body>
-        </Card>
+        </div>
       </div>
       <div>
-      <Modal size = 'lg' show = {showEditDeck} onHide = {()=>setShowEditDeck(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>
-            <div>Delete/Edit Deck</div>
-          </Modal.Title>
-          <Modal.Body>
-            <p>Change Deck Title</p>
-            <input onChange = {(e) => setTitle(e.target.value)} value = {title} type="text"/>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant='success' onClick={()=> {
-              setShowEditDeck(false)
-              props.editDeck(props.deck.id, title, props.deckIndex)}
+        <Modal size='lg' show={showEditDeck} onHide={() => setShowEditDeck(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              <div>Delete/Edit Deck</div>
+            </Modal.Title>
+            <Modal.Body>
+              <p>Change Deck Title</p>
+              <input onChange={(e) => setTitle(e.target.value)} value={title} type="text" />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant='success' onClick={() => {
+                setShowEditDeck(false)
+                props.editDeck(props.deck.id, title, props.deckIndex)
+              }
               }>Save Deck</Button>
-            <Button variant='danger' onClick={()=> {
-              setShowEditDeck(false)
-              props.deleteDeck(props.deck.id, props.deckIndex)}}>Delete Deck</Button>
-          </Modal.Footer>
-        </Modal.Header>
-      </Modal>
+              <Button variant='danger' onClick={() => {
+                setShowEditDeck(false)
+                props.deleteDeck(props.deck.id, props.deckIndex)
+              }}>Delete Deck</Button>
+            </Modal.Footer>
+          </Modal.Header>
+        </Modal>
       </div>
-    </div>
+    </div >
   );
 }
