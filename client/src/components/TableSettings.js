6377bcb802dc17e73dffa11535657be64118047e
ibrahim jabr <ibrahim.aljabr@gmail.com>
2019-08-15 09:46:37 -0500

    working navBar with the add table

--- client/src/components/TableSettings.js
+++ client/src/components/TableSettings.js
@@ -28,15 +28,14 @@ export default function TableSettings(props) {
                 <input onChange = {(e) => setTableName(e.target.value)} value = {tableName} type="text" placeholder = 'Enter table name'/>
               </div>
               <div style = {{paddingTop: '10px'}}></div>
-              <div>Description</div>
-              <div style = {{paddingTop: '10px'}}></div>
-              <textarea style = {{height: '50px'}} onChange = {(e) => setDescName(e.target.value)} value = {descName} type="text" placeholder = 'Enter table name'/>
               <div style = {{paddingTop: '10px'}}></div>
               <div>Invite Players</div>
               <div style = {{paddingTop: '10px'}}></div>
               <div>
-                <input onChange = {(e) => setPlayerName(e.target.value)} type="email" name="emailaddress" placeholder = 'Enter user Email'/>
-                <Button onClick = {() => props.addPlayerToTable(playerName)} style = {{position: 'relative',top: '-1px'}} size = 'sm' variant= 'success'>+</Button>
+                <form>
+                <input onChange = {(e) => setPlayerName(e.target.value)} style = {{display: 'inline' ,width:'75%', marginRight:'5px'}} type="email" name="emailaddress" placeholder = 'Enter user Email'/>
+                <Button onClick = {() => props.addPlayerToTable(playerName)} style = {{display: 'inline', position: 'relative',top: '-1px'}} size = 'sm' variant= 'success'>+</Button>
+                </form>
               </div>
               <div style = {{paddingTop: '10px'}}></div>
               <Button onClick = {() => props.addTable(tableName ,props.newPLayer)} variant= 'success'>Save</Button>
