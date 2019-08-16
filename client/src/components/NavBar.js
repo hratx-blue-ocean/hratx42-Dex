90da53697128f32ee60a6bdd15de9a39bab23256
ibrahim jabr <ibrahim.aljabr@gmail.com>
2019-08-16 17:51:27 -0500

    new button desgin

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,30 +3,36 @@ import { Link, withRouter } from "react-router-dom";
 import TableSettings from './TableSettings';
 import { Navbar, Button, NavDropdown, Nav, Form } from "react-bootstrap";
 function NavBar(props) {
+  console.log(props.userName)
+  let initial='';
+  if(props.userName !== undefined){
+    initial = props.userName.split(" ").map(char => char[0]).join("").toUpperCase()
+  }
+  
   const { history } = props;
   let allTables = props.tables.map((item) => <NavDropdown.Item key={Math.random()} onClick={() => history.push(`/table/${item.id}`)}>{item.name}</NavDropdown.Item>)
   return (
 
     <div>
       <Navbar bg="light" className='dexNavBar' expand="lg" >
-        <Navbar.Brand style={{ marginBottom: '22px' }}>
-          <Link style={{ color: 'black' }} to={"/dashboard"}><h2 style={{ marginLeft: "15px" }}>/Dex</h2></Link>
+        <Navbar.Brand >
+          <Link style={{ color: 'black' }} to={"/dashboard"}><img src='/assets/Logo.png' alt='' style={{ marginLeft: "5px" }}></img></Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto" />
           <Form style={{ marginRight: "25px" }}>
-            <Button variant="outline-primary" className="navBarAddTableBtn" onClick={() => props.changeTableModal()}>
+            <Button  className="navBarPlusCircle" style={{backgroundColor:'#FF5A5F', color:'#f1f0f0'}} onClick={() => props.changeTableModal()}>
               <strong>+</strong>
             </Button>
           </Form>
-          <NavDropdown title="Tables">
+          <NavDropdown  className="navBarUserCircle navBarDropdown" title={<img alt='' style={{color:'black',width: '20px',height:'30px'}} src='/assets/LogoMakr_1QrEWb.png'></img>} style={{ marginRight: "25px"}}>
             {allTables}
             <NavDropdown.Divider />
             <NavDropdown.Item onClick={() => props.changeTableModal()}>Create Table</NavDropdown.Item>
           </NavDropdown>
-          <NavDropdown className="navBarUserCircle" title={props.userName} id="collasible-nav-dropdown" style={{ marginTop: ".125rem", marginRight: "15px", marginBottom: "0", marginLeft: "- 70px" }}>
-            <div style = {{paddingLeft: '30px'}}><Link style = {{color: 'black'}} to={"/dashboard"}>Dashboard</Link></div>
+          <NavDropdown className="navBarUserCircle navBarDropdown" title={initial} id="dropdown-menu-align-right" style={{  marginTop: ".125rem", marginRight: "40px", marginBottom: "0" }}>
+            <div style = {{paddingLeft: '25px'}}><Link style = {{color: 'black', fontWeight: 'lighter'}} to={"/dashboard"}>Dashboard</Link></div>
             <NavDropdown.Divider />
             <NavDropdown.Item onClick={() => props.logOut()}>Log Out</NavDropdown.Item>
           </NavDropdown>
