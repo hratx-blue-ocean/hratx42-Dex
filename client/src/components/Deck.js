abc8ac42457541b9c8e82eca2986d857c67d2279
Brooke <brooke.snellings@gmail.com>
2019-08-16 16:04:51 -0500

    Style Edit Deck button

--- client/src/components/Deck.js
+++ client/src/components/Deck.js
@@ -33,13 +33,13 @@ export default function Deck(props) {
     <div id="deckWrapper">
       <div id="deckContent" style={{ width: '100%' }} >
         <div id="deckHeader" style={{ width: '100%' }}> <span className="deckTitle">{props.deck.title} </span>
-          <Button
+          <div style={{ float: 'right' }}><Button
             className="deckEditBtn"
-            variant='outline-success'
+            variant='link'
             onClick={() => setShowEditDeck(true)}
           >
-            Edit Deck
-              </Button>
+            ...
+              </Button></div>
         </div>
         <div id="deckScrollbar" style={{ overflow: 'scroll' }}>
           <Card.Body className='row'>
@@ -71,7 +71,6 @@ export default function Deck(props) {
               card={props.singleCard}
 
             />
-
             <Button className="deckAddAnotherCardBtn" className='row' variant='outline-success' onClick={() => handleShow()}>
               Add Another Card
             </Button>
