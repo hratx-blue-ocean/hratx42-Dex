b86a955467f441e10a48db567a9ff5ea2342a5b4
hhendrycks <hmhendrycks@gmail.com>
2019-08-17 11:14:59 -0500

    Positioning buttons and username icons floated to separate sides of card thumbnail, increased in size

--- client/src/components/CardThumbnails.js
+++ client/src/components/CardThumbnails.js
@@ -10,7 +10,7 @@ export default function CardThumbnails(props) {
 
 
   return (
-    <div style={{ width: '150px', height: '150px' }}>
+    <div style={{ width: '250px', height: '250px'}}>
 
       <EditCardModal users={props.users}
                     labels={props.labels}
@@ -27,8 +27,9 @@ export default function CardThumbnails(props) {
 
       <Card onClick={() => {
         setShow(true)
-      }} style={{ width: '150px', height: '150px' }} className='CardThumbnailsSingleCard'>
-        <Row>
+      }} style={{ width: '250px', height: '250px' }} className='CardThumbnailsSingleCard'>
+
+        <Row style = {{ overflow: "hidden" }}>
           <Col md={1}>
             <div className='cardThumbnails_impact'> {props.singleCard.weight}/{props.singleCard.impact}</div>
           </Col>
@@ -37,43 +38,44 @@ export default function CardThumbnails(props) {
           </Col>
         </Row>
         <div style={{ fontSize: '12px', paddingLeft: '5px' }}>{props.singleCard.title}</div>
-        <div>
-
-          {/* more users button leads to edit form to view all users */}
-          <OverlayTrigger
-            key={'bottom'}
-            placement={'bottom'}
-            overlay={
-              <Tooltip id={`tooltip-bottom`}>
-                Add/View More
-              </Tooltip>}>
-            <Button
-              key={Math.random()}
-              className='CardThumbnails_userIcon float-right'
-              variant='secondary'
-              onClick={() => {
-                setShow(true)
-              }}
-            ><strong>+</strong>
-            </Button>
-          </OverlayTrigger>
-          {props.singleCard.cards_members.map((member, i) =>
-              <OverlayTrigger
+        <div style = {{ display: "flex", justifyContent: "space-between" }}>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <img className='CardThumbnailsMove' height='25' src="/assets/downButton.png" onClick={() => props.moveCard(props.singleCard, props.cardIndex, props.deckIndex, 1)} />
+            <img className='CardThumbnailsMove' height='25' src="/assets/upButton.png" onClick={() => props.moveCard(props.singleCard, props.cardIndex, props.deckIndex, -1)} />
+          </div>
+          <div style = {{ display: "flex", flexDirection: "row" }}>
+            {/* more users button leads to edit form to view all users */}
+            {props.singleCard.cards_members.map((member, i) =>
+                <OverlayTrigger
+                  key={Math.random()}
+                  placement={'bottom'}
+                  overlay={
+                    <Tooltip id={`tooltip-bottom`}>
+                      {member.member_name}
+                    </Tooltip>
+                  }
+                >
+                  {member.member_name === null ? (<></>) : i < 3 ? (<Button key={Math.random()} className='CardThumbnails_userIcon float-right' variant='secondary'>{member.member_name.split(" ").map(char => char[0]).join("").toUpperCase()}</Button>) : <></>}
+                </OverlayTrigger>
+            )}
+            <OverlayTrigger
+              key={'bottom'}
+              placement={'bottom'}
+              overlay={
+                <Tooltip id={`tooltip-bottom`}>
+                  Add/View More
+                </Tooltip>}>
+              <Button
                 key={Math.random()}
-                placement={'bottom'}
-                overlay={
-                  <Tooltip id={`tooltip-bottom`}>
-                    test
-                  </Tooltip>
-                }
-              >
-                {member.member_name === null ? (<></>) : i < 3 ? (<Button key={Math.random()} className='CardThumbnails_userIcon float-right' variant='secondary'>{member.member_name.split(" ").map(char => char[0]).join("").toUpperCase()}</Button>) : <></>}
-              </OverlayTrigger>
-          )}
-        </div>
-        <div style={{ paddingLeft: '5px', position: 'absolute', top: '80%' }}>
-          <img className='CardThumbnailsMove' height='15' src="/assets/downButton.png" onClick={() => props.moveCard(props.singleCard, props.cardIndex, props.deckIndex, 1)} />
-          <img className='CardThumbnailsMove' height='15' src="/assets/upButton.png" onClick={() => props.moveCard(props.singleCard, props.cardIndex, props.deckIndex, -1)} />
+                className='CardThumbnails_userIcon float-right'
+                variant='secondary'
+                onClick={() => {
+                  setShow(true)
+                }}
+              ><strong>+</strong>
+              </Button>
+            </OverlayTrigger>
+          </div>
         </div>
       </Card>
     </div>
