1843ee5f5a03db64461e7aaf378fe326281f497f
ibrahim jabr <ibrahim.aljabr@gmail.com>
2019-08-17 17:08:06 -0500

    change dashbored layout.
    Co-authored-by: name DJ Park <dongjae93@gmail.com>
    Co-authored-by: name Brooke Snellings <brooke.snellings@gmail.com>
    Co-authored-by: name Laine Doud-Eisenbart <ldoudeisenbart@gmail.com>

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -13,29 +13,43 @@ export default function Dashboard(props) {
   return (
    
     <Row style={{minHeight: '750px' ,maxHeight: '750px'}}>
-      <Col xl={4} lg={4} md={4} sm={4} xs={4} style={{paddingTop:'30'}}>
+      <Col xl={9} lg={9} md={9} sm={9} xs={9} style={{paddingTop:'30',backgroundColor:'#F1F0F0',border:'none'}}>
         {showProfile ? (
           <ProfileEditForm hideProfile={hideProfile.bind(this)} userId={props.userId} user={props.user} />
         ) : (
-          <div style={{minHeight: '750px' ,maxHeight: '750px'}}>
-            <Card style={{minHeight: '750px' ,maxHeight: '750px'}}>
-              <div style = {{marginTop:'40px',fontSize:'24px',textAlign:'center' }}><strong>Welcome back to your dashboard {props.user.name}</strong></div>
-              <hr/>
-              <div style = {{textAlign:'center' }}>Please refer to our <span style = {{textDecoration: 'underline'}}>TOS</span> for any questions</div>
-              <hr/>
+          <div style={{backgroundColor:'#F1F0F0'}}>
+            <Card style={{backgroundColor:'#F1F0F0',border:'none'}}>
+              <div style = {{marginTop:'40px',fontSize:'30px',textAlign:'center' }}><strong>Welcome back to your dashboard {props.user.name}</strong></div>
+               </Card>
+              <Card style={{backgroundColor:'#F1F0F0',border:'none',marginLeft:'auto',marginRight:'auto'}} className="dashboardTables">
+            <div style={{marginLeft:'auto',marginRight:'auto'}} className="dashboardTablesContainer">
+              {props.tables.map((table, index) =>
+                <ul key = {Math.random()}>
+                    <li>
+                      <div style = {{backgroundImage: `url(${images[index % 4]})`}} onClick={() => props.history.push(`/table/${table.id}`)} className="newTable">
+                        <h5>{table.name}</h5>
+                      </div>
+                    </li>
+                </ul>
+              )}
+            </div>
+        </Card>
+              {/* <div style = {{textAlign:'center' }}>Please refer to our <span style = {{textDecoration: 'underline'}}>TOS</span> for any questions</div>
+              
               <div style = {{textAlign:'center' }}>Tables you are a member of: {props.tables.length}</div>
               <div style = {{textAlign:'center' }}>Total Cards owned: {props.cards.length}</div>
-              <div style = {{textAlign:'center' }}>Cards per Table Average: {props.tables.length/5}</div>
-              <div style = {{textAlign:'center', borderRadius:'50%' }}><img style = {{width:'70%'}} alt=''src='/assets/circle-cropped.png' ></img></div>
-              <Button style = {{width:'30%',marginLeft:'30px',marginTop:'50px', backgroundColor:'#60BE4E'}} onClick = {() => setShowProfile(true)}>Edit Profile</Button>
-            </Card>
+              <div style = {{textAlign:'center' }}>Cards per Table Average: {props.tables.length/5}</div> */}
+              {/* <div style = {{textAlign:'center', borderRadius:'50%' }}><img style = {{width:'70%'}} alt=''src='/assets/circle-cropped.png' ></img></div> */}
+              
+           
           </div>
         )}
       </Col>
       {/* tables */} 
-      <Col id='tableAndCards' xl={8} lg={8} md={8} sm={8} xs={8} className="dashboardTables" style={{paddingTop:'30',minHeight: '750px' ,maxHeight: '750px'}}>
-      
-        <Card style={{minHeight: '470px' ,maxHeight: '470px'}}>
+      <Col id='tableAndCards' xl={3} lg={3} md={3} sm={3} xs={3} className="dashboardTables" style={{paddingTop:'30',backgroundColor:'#F1F0F0'}}>
+      <Button style = {{width:'30%',marginLeft:'30px',marginTop:'50px', backgroundColor:'#60BE4E'}} onClick = {() => setShowProfile(true)}>Edit Profile</Button>
+      {/*
+        <Card style={{backgroundColor:'#F1F0F0',border:'none'}}>
           <h2  style={{marginTop:'40px'}}>{props.user.name}'s Tables</h2>
             <div className="dashboardTablesContainer">
               {props.tables.map((table, index) =>
@@ -52,8 +66,8 @@ export default function Dashboard(props) {
         <div style={{paddingTop:'30px'}}>
 
         </div>
-        <Card style={{minHeight: '250px' ,maxHeight: '250px'}}>
-          <div>
+         <Card style={{minHeight: '250px' ,maxHeight: '250px',backgroundColor:'#F1F0F0'}}>
+          <div style={{backgroundColor:'#F1F0F0'}}>
             <h2 style={{marginTop:'40px'}}>{props.user.name}'s Cards</h2>
               <div>
               {props.cards.map((singleCard, cardIndex) =>
@@ -70,7 +84,7 @@ export default function Dashboard(props) {
               </div>
           </div>
         </Card>
-       
+        */}
       </Col>
     </Row>
    
