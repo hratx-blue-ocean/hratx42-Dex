9f22f27a0f7ebc976d261911ae49fffd3ae0a3d7
Michael Bergeron <michaelbergeron@Michaels-MacBook-Pro.local>
2019-08-17 14:21:00 -0500

    added labels and styled the header of each card

---
+++ client/src/components/CardHeader.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+
+export default function CardHeader(props) {
+  const labels = [];
+  let subLabels = [];
+  for (let i = 0; i < props.labels.length; i++){
+    subLabels.push(props.labels[i]);
+    if (i % 3 === 0 && i > 1) {
+      labels.push(subLabels);
+      subLabels = [];
+    }
+    if (i === props.labels.length - 1 && i % 3 !== 0){
+      labels.push(subLabels)
+    }
+  }
+  return (
+    <div>
+      <Row className = 'no-gutters' style = {{ overflow: "hidden" }}>
+        <Col md={3}>
+          <div className='cardThumbnails_impact'>{props.weight} | {props.impact}</div>
+        </Col>
+        <Col className = 'no-gutters' md = {9}>
+          {labels.map((subLabel) =>
+            <Row key={Math.random()} className = 'no-gutters' style = {{paddingTop: '0px', paddingBottom: '0px'}}>
+              {subLabel.map((label) => 
+              <Col key={Math.random()} md = {4}>
+                <div className = 'cardHeaderLabels' style={{ height: '11px', fontSize: '8px', backgroundColor: `${label.color}`, borderRadius: '10%' }}>{label.label_name}</div>
+              </Col>
+              )}
+            </Row>
+          )}
+        </Col>
+      </Row>
+    </div>
+  )
+}
